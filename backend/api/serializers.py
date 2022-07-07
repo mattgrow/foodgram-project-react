3d0c6c149efdad44405e8a8944f7ca6b741f48b9
mattgrow <bespalov-sd@yandex.ru>
2022-07-07 11:50:30 +0300

    second test push

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,8 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer
-
 from rest_framework import serializers
+
 from recipes.models import (Favorite, Follow, Ingredient, Recipe,
                             RecipeIngredientQty, Shopping, Tag)
 from users.models import User
