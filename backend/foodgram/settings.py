b53c2050bd2b7a33a865f335cb2586899588d557
mattgrow <bespalov-sd@yandex.ru>
2022-07-08 16:15:50 +0300

    second commit to server

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.250.74.253','127.0.0.1', 'localhost', 'backend']
+ALLOWED_HOSTS = ['51.250.74.253', '127.0.0.1', 'localhost', 'backend']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
