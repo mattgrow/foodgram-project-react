654e367180a4d2ad472a7b195f74e09e38885ae6
mattgrow <bespalov-sd@yandex.ru>
2022-07-11 21:33:40 +0300

    fifth attempt

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -4,6 +4,6 @@ from rest_framework import permissions
 class CurrentUserOrAdminOrReadOnly(permissions.IsAuthenticated):
     def has_object_permission(self, request, view, obj):
         user = request.user
-        if obj.author == user or user.role == user.ADMIN:
-            return True
-        return request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS 
+                or obj.author == user
+        )
