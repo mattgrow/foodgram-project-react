29fb41bc5e6570deac0bd07fc8c3b37971fa4cca
mattgrow <bespalov-sd@yandex.ru>
2022-07-12 08:28:24 +0300

    after review

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,10 +1,19 @@
 from rest_framework import permissions
 
 
-class CurrentUserOrAdminOrReadOnly(permissions.IsAuthenticated):
+class CurrentUserOrAdminOrReadOnly(permissions.BasePermission):
+    
+    def has_permission(self, request, view):
+        user = request.user
+        return (
+            request.method in permissions.SAFE_METHODS
+            or user.is_authenticated
+        )
+    
     def has_object_permission(self, request, view, obj):
         user = request.user
         return (
             request.method in permissions.SAFE_METHODS
             or obj.author == user
+            or user.is_superuser
         )
